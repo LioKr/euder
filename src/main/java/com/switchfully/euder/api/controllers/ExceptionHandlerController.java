22dd6afc13f1383443bbd40ea519f70a9de65b41
LioKr <lio.krebs@gmail.com>
2021-03-30 13:05:15 +0200

    00 | added ExceptionHandlerController

---
+++ src/main/java/com/switchfully/euder/api/controllers/ExceptionHandlerController.java
@@ -0,0 +1,23 @@
+package com.switchfully.euder.api.controllers;
+
+import com.switchfully.euder.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@ControllerAdvice
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerController.class);
+
+    @ExceptionHandler
+    public void handleIllegalArgumentException(IllegalArgumentException exception, HttpServletResponse response) throws IOException {
+        LOGGER.warn(exception.getMessage(), exception);
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
+    }
+}
