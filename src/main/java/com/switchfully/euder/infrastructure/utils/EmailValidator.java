b80969ce7086b269d0103aa541fe1fc148c66af5
LioKr <lio.krebs@gmail.com>
2021-04-01 10:30:59 +0200

    fix | Validator

--- src/main/java/com/switchfully/euder/infrastructure/utils/EmailValidator.java
+++ src/main/java/com/switchfully/euder/infrastructure/utils/EmailValidator.java
@@ -1,39 +1,38 @@
 package com.switchfully.euder.infrastructure.utils;
 
-import com.switchfully.euder.domain.entities.user.Email;
-import com.switchfully.euder.domain.entities.user.User;
-import com.switchfully.euder.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.util.regex.Pattern;
 
-@Component
 public final class EmailValidator {
     private static final String EMAIL_VALID_FORMAT = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
     private static final Pattern EMAIL_VALID_PATTERN = Pattern.compile(EMAIL_VALID_FORMAT, Pattern.CASE_INSENSITIVE);
-    private static UserService userService; //Question is this ok? it works but feels bad
+   /* private static UserService userService; //Question is this ok? it works but feels bad
 
     @Autowired
     public EmailValidator(UserService userService) {
         this.userService = userService;
     }
 
+    */
+
     public static boolean isValidEmail(String email) {
-        //return EMAIL_VALID_PATTERN.matcher(email).matches();
-        return (EMAIL_VALID_PATTERN.matcher(email).matches()
+        return EMAIL_VALID_PATTERN.matcher(email).matches();
+      /*  return (EMAIL_VALID_PATTERN.matcher(email).matches()
                 && isNewEmail(email)
         );
 
+       */
+
 
     }
-
+/*
     private static boolean isNewEmail(String email) {
         return userService.getAllUser().stream()
                 .map(User::getEmail)
                 .map(Email::getMailAddress)
                 .noneMatch(mailAddress -> mailAddress.toLowerCase().equals(email.toLowerCase()));
     }
+
+ */
 
 
 }
--- src/main/java/com/switchfully/euder/infrastructure/utils/PhoneNumberValidator.java
+++ src/main/java/com/switchfully/euder/infrastructure/utils/PhoneNumberValidator.java
@@ -1,34 +1,34 @@
 package com.switchfully.euder.infrastructure.utils;
 
-import com.switchfully.euder.domain.entities.user.PhoneNumber;
-import com.switchfully.euder.domain.entities.user.User;
-import com.switchfully.euder.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.util.regex.Pattern;
 
-@Component
 public final class PhoneNumberValidator {
     private static final String PHONE_VALID_FORMAT = "^((?:\\+|00)[17](?: |\\-)?|(?:\\+|00)[1-9]\\d{0,2}(?: |\\-)?|(?:\\+|00)1\\-\\d{3}(?: |\\-)?)?(0\\d|\\([0-9]{3}\\)|[1-9]{0,3})(?:((?: |\\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\\-)[0-9]{3}(?: |\\-)[0-9]{4})|([0-9]{7}))";
-    private static UserService userService; //Question is this ok? it works but feels bad
-
+    //private static UserService userService; //Question is this ok? it works but feels bad
+/*
     @Autowired
     public PhoneNumberValidator(UserService userService) {
         this.userService = userService;
     }
 
-    public static boolean isValidPhoneNumber(String inputPhoneNumber) {
-        return (Pattern.matches(PHONE_VALID_FORMAT, inputPhoneNumber)
-                && isNewPhoneNumber(inputPhoneNumber));
-    }
+ */
 
+    public static boolean isValidPhoneNumber(String inputPhoneNumber) {
+        return Pattern.matches(PHONE_VALID_FORMAT, inputPhoneNumber);
+        /*return (Pattern.matches(PHONE_VALID_FORMAT, inputPhoneNumber)
+                && isNewPhoneNumber(inputPhoneNumber));
+
+         */
+    }
+/*
     private static boolean isNewPhoneNumber(String inputPhoneNumber) {
         return userService.getAllUser().stream()
                 .map(User::getPhoneNumber)
                 .map(PhoneNumber::getPhoneNumber)
                 .noneMatch(phoneNumber -> phoneNumber.equals(inputPhoneNumber));
     }
+
+ */
 }
 
 
